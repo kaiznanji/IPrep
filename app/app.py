a9cc469fd160d3686ae7e4759dbf76b1856ae94f
Kaiz Nanji <kaiznanji@gmail.com>
2021-01-23 22:08:08 -0500

    Kaiz Upload

--- app/app.py
+++ app/app.py
@@ -4,31 +4,33 @@ from nlp_methods import get_similarity_score
 
 app = Flask(__name__)
 
-
-@app.route('/contact', methods=['GET', 'POST'])
+#Pages
+@app.route('/contact')
 def contact():
     return render_template("contact.html")
+ 
 
-
-
-@app.route('/about', methods=['GET', 'POST'])
+@app.route('/about') 
 def about():
     return render_template("about.html")
 
-
-@app.route('/room', methods=['GET', 'POST'])
+ 
+@app.route('/room')
 def room():
     return render_template("room.html")
 
-
-@app.route('/results', methods=['GET', 'POST'])
+@app.route('/results')
 def results():
     return render_template("results.html")
 
-
-
-
-# Main page
+# POST AND GET REQUESTS 
+@app.route('/recievetext', methods=['GET','POST'])
+def recieve_blob():
+    if request.method == 'POST':
+        print(request.get_json())
+    return 'Call from get' 
+  
+# Main page   
 @app.route('/')
 def index():
     return render_template("main.html")
